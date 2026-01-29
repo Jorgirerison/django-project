c60ef02506e032f8a159dfddb0ac60df3611f4ef
jorgirerison <jorgipeixoto@alu.ufc.br>
2026-01-29 09:52:36 -0300

    removendo outras rotas

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,11 +5,3 @@ def home(request):
     # name_space para não colidir com
     # outros arquivos
     return render(request, "recipes/home.html", context={"name": "Jorgirerison"})
-
-
-def contato(request):
-    return HttpResponse("contato")
-
-
-def sobre(request):
-    return HttpResponse("sobre")
