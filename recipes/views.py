889138fb689e7733858139ce36ae2f7e91b834e6
jorgirerison <jorgipeixoto@alu.ufc.br>
2026-01-29 09:29:16 -0300

    retornando variáveis no front

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def home(request):
     # name_space para não colidir com
     # outros arquivos
-    return render(request, "recipes/home.html")
+    return render(request, "recipes/home.html", context={"name": "Jorgirerison"})
 
 
 def contato(request):
